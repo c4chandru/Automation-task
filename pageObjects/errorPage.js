adb22fb59913305a3246a7caa61d8f9cef223ac8
c4chandru <c4chandrasekar@gmail.com>
2019-07-31 01:31:18 +0530

    added comments on the test cases

--- pageObjects/errorPage.js
+++ pageObjects/errorPage.js
@@ -11,8 +11,6 @@ class fooPage {
             backToHome: '//button'
 
         }
-
-
     }
 
     validateFooPage(){
--- test/appTest.js
+++ test/appTest.js
@@ -5,6 +5,10 @@ var fooPage = require('../pageObjects/errorPage');
 
 describe('Testing job listing page and job details page', function () {
 
+    /*
+    When users open the job list, they can see a list of 5 jobs with pictures, company names and locations.
+     */
+
     it('launch browser, validate title and job listings', function () {
         common.openBrowserLaunchUrl();
         common.validatePageTitle();
@@ -12,6 +16,11 @@ describe('Testing job listing page and job details page', function () {
         homePage.homePageValidateImage();
 
     });
+
+    /*
+    After users click on a job from the list, they can see job details.
+    Users can navigate back from a job details page to the job list.
+     */
 
     it('click on a job list and assert the details', function(){
         common.openBrowserLaunchUrl();
@@ -22,6 +31,10 @@ describe('Testing job listing page and job details page', function () {
         jobPage.clickOnBack();
         common.validatePageTitle();
     });
+
+    /*
+    Users see error message when they visit an unknown URL e.g. http://localhost:3333/foo.
+     */
 
     it('open /foo endpoint and validate the page details', function () {
         common.launchFooUrl();
